a22166b27ffa97db1658c1eff52d05d30c4d9e9c
AbdulKhaliq Alhassan <89224810+AbdullKhalq@users.noreply.github.com>
2024-02-17 16:14:02 +0300

    Add files via upload

---
+++ Lecture 7 challenge Easy.cpp
@@ -0,0 +1,21 @@
+/*
+  This code asks a user to enter a number and print multiplication table of that
+  number up to 10.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  cout << "Enter number: ";
+  int num;
+  cin >> num;
+
+  for (int counter = 1; counter <= 10; counter++) 
+  {
+    cout << num << " * " << counter << " = " << counter * num << "\n";
+  }
+
+  return 0;
+}
