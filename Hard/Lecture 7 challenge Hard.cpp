f3858bd5dd38d4f3229b98b1978d012555188e91
AbdulKhaliq Alhassan <89224810+AbdullKhalq@users.noreply.github.com>
2024-02-17 16:20:25 +0300

    Add files via upload

---
+++ Hard/Lecture 7 challenge Hard.cpp
@@ -0,0 +1,37 @@
+/*
+  Ask a user to enter an odd number N and print diamond pattern with N rows.
+  
+  How the code works:
+    - Takes odd number from user, if it's even or n < 2, it asks again. 
+    - Then prints '*' mulitplied by the odd numbers from 1 to N. 
+    - To make them centered, it prints spaces before and after '*'
+      calculated by N - i / 2.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  cout << "Enter rows (odd): ";
+  int num;
+  cin >> num;
+
+  while (num % 2 == 0 || num < 2) 
+  {
+    cout << "Invalid number\n";
+    cout << "Enter rows (odd): ";
+    cin >> num;
+  }
+  for (int i = 1; i < num; i += 2)
+  {
+    cout << string((num - i) / 2, ' ') << string(i, '*')
+         << string((num - i) / 2, ' ') << '\n';
+  }
+  for (int i = num; i >= 1; i -= 2)
+  {
+    cout << string((num - i) / 2, ' ') << string(i, '*')
+         << string((num - i) / 2, ' ') << '\n';
+  }
+  return 0;
+}
---
+++ Medium/Lecture 7 challenge Medium.cpp
@@ -0,0 +1,36 @@
+/*
+  Asks a user to enter a number and check whether it is a prime number or not.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+  cout << "Enter Number: ";
+  int num;
+  cin >> num;
+
+  bool isPrime = true;
+  
+  if (num <= 1)  // 1 or less is not a prime number
+      isPrime = false;
+  
+  // Check from 2 to n-1
+  // if any number divides n with zero remainder, then n is not prime.
+  for (int i = 2; i <= num / 2; i++)  
+  {
+      if (num % i == 0)
+          isPrime = false;
+
+  }
+  if(isPrime == true)
+    cout << "Prime Number";
+  else
+    cout << "Not a Prime Number";
+
+
+  return 0;
+}
